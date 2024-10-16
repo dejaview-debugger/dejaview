8823affd7989159c74a4752b52ad41c8d3d9d4c2
wengh <wenghy02@gmail.com>
2024-10-16 02:04:11 -0400

    step back

--- counting/counting.py
+++ counting/counting.py
@@ -5,22 +5,34 @@ import types
 import pdb
 from dataclasses import dataclass
 
+
 class FrameCounter:
-    excluded_prefixes = (sys.prefix, os.path.dirname(os.__file__), "<frozen", "<string>")
+    excluded_prefixes = (
+        sys.prefix,
+        os.path.dirname(os.__file__),
+        "<frozen",
+        "<string>",
+    )
     debugger_files = (pdb.__file__,)
-    instance = []
 
     class RerunException(Exception):
         pass
 
-    def __init__(self, func, handler=None):
+    def __init__(self, func):
         self.func = func
-        self.handler = handler or self.print_handler
-        self.breakpoint_count = -1
-        self.breakpoint_handler = None
+        self.handlers = []
         self.count = 0
         self.sub_tracer = None
-        self.own_function_codes = (self.settrace.__code__, self.get_tracer.__code__, CustomPdb.__init__.__code__)
+        self.own_function_codes = (
+            self.settrace.__code__,
+            self.breakpointhook.__code__,
+        )
+        self.skipped_frames = []
+        self.pdb = None
+        self.allow_breakpoints = True
+
+    def add_handler(self, handler):
+        self.handlers.append(handler)
 
     def settrace(self, func):
         self.sub_tracer = func
@@ -33,22 +45,31 @@ class FrameCounter:
             frame = frame.f_back
 
     def should_skip_frame_recursively(self, frame: types.FrameType) -> bool:
-        """"Determine if the frame and all its subframes should be skipped."""
-        return frame.f_code in self.own_function_codes \
-            or frame.f_code.co_filename in self.debugger_files \
+        """ "Determine if the frame and all its subframes should be skipped."""
+        return (
+            frame.f_code in self.own_function_codes
+            or frame.f_code.co_filename in self.debugger_files
             or frame.f_code.co_filename.startswith(self.excluded_prefixes)
+        )
 
     def get_tracer(self, sub_tracer):
         def tracer(frame: types.FrameType, event: str, arg: any) -> any:
-            # Skip frames that are part of the standard library or the debugger
+            # Skip frames that should be skipped
+            while self.skipped_frames:
+                if frame.f_back == self.skipped_frames[-1]:
+                    self.skipped_frames.append(frame)
+                    return None  # Returning None makes us skip the current function but not calls made from it
+                self.skipped_frames.pop()
+
             if self.should_skip_frame_recursively(frame):
+                self.skipped_frames.append(frame)
                 return None
 
             try:
-                # Call the user-defined handler
-                self.handler(self.count, frame, event, arg)
-                if self.count == self.breakpoint_count:
-                    self.breakpoint_handler()
+                # Call the user-defined handlers, remove the ones that return True
+                self.handlers = [
+                    h for h in self.handlers if not h(self.count, frame, event, arg)
+                ]
 
                 # Call the sub-tracer if it exists
                 actual_sub_tracer = self.sub_tracer or sub_tracer
@@ -60,77 +81,91 @@ class FrameCounter:
                 return tracer
             finally:
                 self.count += 1
+
         return tracer
 
-    def rerun_to(self, count: int, breakpoint_handler):
-        self.breakpoint_count = count
-        self.breakpoint_handler = breakpoint_handler
+    def rerun_to(self, to_count: int):
+        def handler(count, frame, event, arg):
+            if count == to_count:
+                self.allow_breakpoints = True
+                self.get_pdb().set_trace(frame)
+                return True
+            return False
+
+        self.add_handler(handler)
+        self.allow_breakpoints = False
         raise self.RerunException()
 
     def run(self):
         # Backup system tracer and settrace
-        self.old_tracer = sys.gettrace()
+        old_tracer = sys.gettrace()
         self.old_settrace = sys.settrace
+        old_breakpointhook = sys.breakpointhook
         sys.settrace = self.settrace
-        FrameCounter.instance.append(self)
         try:
             while True:
                 try:
                     self.setup()
                     self.func()
                     break
-                except self.RerunException as e:
+                except self.RerunException:
                     pass
         finally:
             # Restore the original tracer and settrace
-            self.old_settrace(self.old_tracer)
+            self.old_settrace(old_tracer)
             sys.settrace = self.old_settrace
-            FrameCounter.instance.remove(self)
+            sys.breakpointhook = old_breakpointhook
 
     def setup(self):
         self.count = 0
+        self.sub_tracer = None
         self.base_frame = sys._getframe().f_back
+        sys.breakpointhook = self.breakpointhook
         self.old_settrace(self.get_tracer(None))
         return self
 
-    @staticmethod
-    def print_handler(count: int, frame: types.FrameType, event: str, arg: any):
-        code = frame.f_code
-        func_name = code.co_name
-        file = code.co_filename
-        line_no = frame.f_lineno
-        print(f"#{count} {event} {func_name}() at line {line_no} of {file}")
+    def get_pdb(self):
+        if self.pdb is None:
+            self.pdb = self.CustomPdb(self)
+        return self.pdb
 
-class CustomPdb(pdb.Pdb):
-    def __init__(self):
-        super().__init__()
-        if FrameCounter.instance:
-            self.counter = FrameCounter.instance[-1]
-        else:
-            self.counter = FrameCounter(None)
+    def breakpointhook(self, *args, **kws):
+        if self.allow_breakpoints:
+            self.get_pdb().set_trace(sys._getframe().f_back)
 
-    def do_back(self, arg):
-        if not arg:
-            arg = 1
-        count = self.counter.count - int(arg)
-        if not 0 <= count < self.counter.count:
-            print("Invalid count")
-            return
-        self.counter.rerun_to(count, self.set_trace)
+    class CustomPdb(pdb.Pdb):
+        def __init__(self, counter):
+            super().__init__()
+            self.counter = counter
 
-    def do_count(self, arg):
-        print(self.counter.count)
+        def do_back(self, arg):
+            if not arg:
+                arg = 1
+            elif not arg.isdigit():
+                print("Invalid argument, must be a number")
+                return
+            count = self.counter.count - int(arg)
+            if not 0 <= count < self.counter.count:
+                print(f"Invalid count, must be between 1 and {self.counter.count}")
+                return
+            self.quitting = True
+            self.counter.rerun_to(count)
+
+        def do_count(self, arg):
+            print(self.counter.count)
+
 
 def foo():
     print("foo")
+
 
 def test():
     print("start")
     for i in range(10):
         if i == 8:
             print("breakpoint 1")
-            CustomPdb().set_trace()
-            print("after breakpoint 1") # should stop here
+            breakpoint()
+            print("after breakpoint 1")  # should stop here
         print(i)
         foo()
         print("random", i)
@@ -140,14 +175,25 @@ def test():
     print("end3")
     return 42
 
+
+def print_handler(count: int, frame: types.FrameType, event: str, arg: any):
+    code = frame.f_code
+    func_name = code.co_name
+    file = code.co_filename
+    line_no = frame.f_lineno
+    print(f"#{count} {event} {func_name}() at line {line_no} of {file}")
+
+
 def handler(count, frame, event, arg):
     FrameCounter.print_handler(count, frame, event, arg)
-    # if count == 23:
-    #     print("breakpoint 2")
-    #     breakpoint()
-    #     print("after breakpoint 2") # should not stop here
+    if count == 23:
+        print("breakpoint 2")
+        breakpoint()
+        print("after breakpoint 2")  # should not stop here
 
-counter = FrameCounter(test, handler)
+
+counter = FrameCounter(test)
+counter.add_handler(print_handler)
 counter.run()
 # test()
 
