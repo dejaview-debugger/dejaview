dbcb91633d1ad49547d5e023115e5ceec5944450
Haoyu Weng <wenghy02@gmail.com>
2024-10-25 21:22:48 -0400

    change patching to use state store

--- patching/patching.py
+++ patching/patching.py
@@ -1,36 +1,37 @@
 import inspect
-import logging
-import types
+import math
 import random
+import types
 from functools import wraps
 
-import math
+from state_store import StateStore
 
-from collections import defaultdict
 
 # Decorator to log function results
 def log_results(func):
-    log_results.stored_res = defaultdict(list)
-    log_results.current_seq = defaultdict(int) # func to int
-    log_results.mode = True # Play vs replay
+    current_seq = -1  # first sequence number should be 0
+    log_results.mode = True  # Play vs replay
+
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if log_results.mode:
-            result = func(*args, **kwargs)  # Execute the function
-            log_results.stored_res[func].append(result)
+        nonlocal current_seq
+        current_seq += 1
+        if log_results.mode: # TODO: switch to enum
+            result = func(*args, **kwargs) # Execute the function
+            StateStore.get(func).set_state(current_seq, result)
             return result
-        # Assume we magically have seq number
-        seq_num = log_results.current_seq[func]
-        log_results.current_seq[func] += 1
-        return log_results.stored_res[func][seq_num]
+        return StateStore.get(func).get_state(current_seq)
+
     return wrapper
+
 
 # Function to apply logging to all functions in a module
 def patch_all_functions_in_module(module):
     for name in dir(module):
         obj = getattr(module, name)
-        if isinstance(obj, types.FunctionType) and name != 'log_results':
+        if isinstance(obj, types.FunctionType) and name != "log_results":
             setattr(module, name, log_results(obj))  # Decorate only real functions
+
 
 # Patches a function
 def patch_func(func):
@@ -45,16 +46,20 @@ def patch_func(func):
     except:
         pass
 
+
 # Example functions to test
 def add(a, b):
     return a + b
 
+
 def multiply(a, b):
     return a * b
+
 
 # Patching all functions in the current module
 if __name__ == "__main__":
     import sys
+
     patch_func(math.sin)
     patch_func(random.random)
     patch_all_functions_in_module(sys.modules[__name__])
@@ -72,7 +77,7 @@ if __name__ == "__main__":
     # Turn on replay mode
     log_results.mode = False
 
-    print(add(0, 0), multiply(0, 0), math.sin(1)) # Should be 8 10 0
+    print(add(0, 0), multiply(0, 0), math.sin(1))  # Should be 8 10 0
 
     # Turn on loggin mode
     log_results.mode = True
@@ -80,16 +85,16 @@ if __name__ == "__main__":
     val1 = random.random()
     val2 = random.random()
 
-    random.seed(0) # Reset
+    random.seed(0)  # Reset
     initial = random.random()
 
     print(val1, val2, "initial", initial)
 
     log_results.mode = False
 
-    random.seed(0) # Reset
-    print(random.random(), random.random()) # Identical line as before
+    random.seed(0)  # Reset
+    print(random.random(), random.random())  # Identical line as before
 
     log_results.mode = True
 
-    print(random.random()) # should be initial
+    print(random.random())  # should be initial
